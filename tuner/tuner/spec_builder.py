2c163954004f0dd0b4023648ccc2e3ee7512ac29
Bangtian Liu <liubangtian@gmail.com>
2025-03-06 23:23:34 -0500

    [tuner] add default attribute to tuning spec (#1016)
    This PR adds the default attribute
    `iree_codegen.tuning_spec_with_default_entrypoint` to the generated
    tuning spec.
    
    fixes #816
    
    Signed-off-by: Bangtian Liu <liubangtian@gmail.com>

--- tuner/tuner/spec_builder.py
+++ tuner/tuner/spec_builder.py
@@ -18,7 +18,7 @@ from .op_matchers import ROOT_OP_ATTR_NAME
 
 def get_placeholder_spec(context: ir.Context) -> ir.Module:
     spec_text = f"""
-        module attributes {{ transform.with_named_sequence }} {{
+        module attributes {{ transform.with_named_sequence, iree_codegen.tuning_spec_with_default_entrypoint }} {{
             transform.named_sequence
             @__kernel_config(%variant_op: !transform.any_op {{transform.readonly}}) -> !transform.any_op
                 attributes {{ iree_codegen.tuning_spec_entrypoint }} {{
@@ -76,7 +76,7 @@ def build_td_spec(
         captured_values.add(operand)
     bbargs_str = ", ".join(bbargs)
     spec_text = f"""
-        module attributes {{ transform.with_named_sequence }} {{
+        module attributes {{ transform.with_named_sequence, iree_codegen.tuning_spec_with_default_entrypoint }} {{
             // Annotation Transform
             transform.named_sequence @apply_op_config(%op: !transform.any_op {{transform.readonly}},
                                                         %config: !transform.any_param {{transform.readonly}}) {{
