43e554503b9c1a39207bfddff550c2bc5f10c133
Chi_Liu <22491986+AmosLewis@users.noreply.github.com>
2025-04-30 16:19:08 -0700

    Delete llama 70b iree compile xfail (#1353)
    Delete the iree compile xfail bring in
    https://github.com/nod-ai/shark-ai/pull/1285/files
    It's been fixed by iree0429
    https://github.com/iree-org/iree/issues/20581#issuecomment-2840800824
    The iree benchmark xfail will stay with error code 5 typically indicates
    an "Access Denied" issue. https://github.com/nod-ai/shark-ai/issues/1355

--- sharktank/tests/models/llama/benchmark_amdgpu_test.py
+++ sharktank/tests/models/llama/benchmark_amdgpu_test.py
@@ -668,9 +668,9 @@ class BenchmarkLlama3_1_70B(BaseBenchmarkTest):
         )
 
     @pytest.mark.xfail(
-        run=False,
-        reason="https://github.com/iree-org/iree/issues/20581",
-        raises=IreeCompileException,
+        reason="https://github.com/nod-ai/shark-ai/issues/1355",
+        strict=True,
+        raises=IreeBenchmarkException,
     )
     def testBenchmark70B_f16_TP8_Non_Decomposed_Input_Len_128(self):
         output_file_name = self.dir_path_70b / "f16_torch_128_tp8"
@@ -720,9 +720,9 @@ class BenchmarkLlama3_1_70B(BaseBenchmarkTest):
         )
 
     @pytest.mark.xfail(
-        run=False,
-        reason="https://github.com/iree-org/iree/issues/20581",
-        raises=IreeCompileException,
+        reason="https://github.com/nod-ai/shark-ai/issues/1355",
+        strict=True,
+        raises=IreeBenchmarkException,
     )
     def testBenchmark70B_f16_TP8_Non_Decomposed_Input_Len_2048(self):
         output_file_name = self.dir_path_70b / "f16_torch_2048_tp8"
