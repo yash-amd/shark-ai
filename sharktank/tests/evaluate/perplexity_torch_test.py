312058d0fffee4d79b1726b903a19701bc3e00d5
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2025-05-08 18:05:03 -0400

    Refactor Torch PPL Tests (#1413)
    Signed-off-by: Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>

--- sharktank/tests/evaluate/perplexity_torch_test.py
+++ sharktank/tests/evaluate/perplexity_torch_test.py
@@ -4,6 +4,7 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from typing import Iterable
 import unittest
 import pytest
 import json
@@ -28,26 +29,24 @@ class PerplexityTest(unittest.TestCase):
     def setUp(self):
         self.current_perplexity_all = {}
         self.delta = 5e-1
-        self.tensor_parallelism_size = 8
+        self.tensor_parallelism_size = 1
         with open(self.baseline_perplexity_scores, "r") as f:
             self.baseline_perplexity = json.load(f)
 
-    def test_llama3_8B_f16(self):
+    def prepare_argv(self, extra_args: Iterable | None = None):
+        self.argv = [
+            f"--irpa-file={self.irpa_file}",
+            f"--tokenizer-config-json={self.tokenizer}",
+            f"--num-prompts={self.batch_size}",
+            f"--device={self.device}",
+            f"--tensor-parallelism-size={self.tensor_parallelism_size}",
+        ]
+        if extra_args:
+            self.argv.extend(extra_args)
 
-        # Llama 3.1 8B non-decomposed
-
-        model_name = "llama3_8B_f16_torch"
-        baseline_perplexity = self.baseline_perplexity[model_name]
-
-        current_perplexity = perplexity_torch.main(
-            [
-                f"--irpa-file={self.llama3_8b_f16_model}",
-                f"--tokenizer-config-json={self.llama3_8b_tokenizer}",
-                f"--attention-kernel=torch",
-                f"--num-prompts={self.batch_size}",
-                f"--device={self.device}",
-            ]
-        )
+    def run_and_check_perplexity(self):
+        baseline_perplexity = self.baseline_perplexity[self.model_name]
+        current_perplexity = perplexity_torch.main(self.argv)
 
         baseline_mean_perplexity = round(
             np.mean(baseline_perplexity["perplexities"][0 : self.batch_size]), 6
@@ -63,114 +62,57 @@ class PerplexityTest(unittest.TestCase):
             msg=f"Current perplexity deviates baseline by {perplexity_difference}",
         )
         gc.collect()
+
+    def test_llama3_8B_f16(self):
+        # Llama 3.1 8B non-decomposed
+        self.model_name = "llama3_8B_f16_torch"
+        self.irpa_file = self.llama3_8b_f16_model
+        self.tokenizer = self.llama3_8b_tokenizer
+
+        self.prepare_argv()
+        self.run_and_check_perplexity()
 
     def test_llama3_8B_f8(self):
-
         # Llama 3.1 8B non-decomposed
+        self.model_name = "llama3_8B_f8_torch"
+        self.irpa_file = self.llama3_8b_f8_model
+        self.tokenizer = self.llama3_8b_tokenizer
 
-        model_name = "llama3_8B_f8_torch"
-        baseline_perplexity = self.baseline_perplexity[model_name]
-
-        current_perplexity = perplexity_torch.main(
-            [
-                f"--irpa-file={self.llama3_8b_f8_model}",
-                f"--tokenizer-config-json={self.llama3_8b_tokenizer}",
-                f"--attention-kernel=torch",
-                f"--num-prompts={self.batch_size}",
-                f"--attention-dtype=bfloat16",
-                f"--activation-dtype=bfloat16",
+        self.prepare_argv(
+            extra_args=(
+                "--attention-dtype=bfloat16",
+                "--activation-dtype=bfloat16",
                 "--use-hf",
                 "--fake-quant",
-                f"--device={self.device}",
-            ]
+            )
         )
-
-        baseline_mean_perplexity = round(
-            np.mean(baseline_perplexity["perplexities"][0 : self.batch_size]), 6
-        )
-        current_mean_perplexity = round(current_perplexity["mean_perplexity"], 6)
-
-        perplexity_difference = current_mean_perplexity - baseline_mean_perplexity
-
-        self.assertAlmostEqual(
-            baseline_mean_perplexity,
-            current_mean_perplexity,
-            delta=self.delta,
-            msg=f"Current perplexity deviates baseline by {perplexity_difference}",
-        )
-        gc.collect()
+        self.run_and_check_perplexity()
 
     @pytest.mark.xfail(
         reason="Non-decomposed attention is not supported yet",
     )
     def test_llama3_405B_f16(self):
-
         # Llama 3.1 405B non-decomposed
+        self.model_name = "llama3_405B_f16_torch"
+        self.irpa_file = self.llama3_405b_f16_model
+        self.tokenizer = self.llama3_405b_tokenizer
+        self.tensor_parallelism_size = 8
 
-        model_name = "llama3_405B_f16_torch"
-        baseline_perplexity = self.baseline_perplexity[model_name]
-
-        current_perplexity = perplexity_torch.main(
-            [
-                f"--irpa-file={self.llama3_405b_f16_model}",
-                f"--tokenizer-config-json={self.llama3_405b_tokenizer}",
-                f"--tensor-parallelism-size={self.tensor_parallelism_size}",
-                f"--attention-kernel=torch",
-                f"--num-prompts={self.batch_size}",
-                f"--device={self.device}",
-            ]
-        )
-
-        baseline_mean_perplexity = round(
-            np.mean(baseline_perplexity["perplexities"][0 : self.batch_size]), 6
-        )
-        current_mean_perplexity = round(current_perplexity["mean_perplexity"], 6)
-
-        perplexity_difference = current_mean_perplexity - baseline_mean_perplexity
-
-        self.assertAlmostEqual(
-            baseline_mean_perplexity,
-            current_mean_perplexity,
-            delta=self.delta,
-            msg=f"Current perplexity deviates baseline by {perplexity_difference}",
-        )
-        gc.collect()
+        self.prepare_argv()
+        self.run_and_check_perplexity()
 
     @pytest.mark.xfail(
         reason="Non-decomposed attention is not supported yet",
     )
     def test_llama3_405B_f8(self):
-
         # Llama 3.1 405B non-decomposed
+        self.model_name = "llama3_405B_f8_torch"
+        self.irpa_file = self.llama3_405b_f8_model
+        self.tokenizer = self.llama3_405b_tokenizer
+        self.tensor_parallelism_size = 8
 
-        model_name = "llama3_405B_f8_torch"
-        baseline_perplexity = self.baseline_perplexity[model_name]
-
-        current_perplexity = perplexity_torch.main(
-            [
-                f"--irpa-file={self.llama3_405b_f8_model}",
-                f"--tokenizer-config-json={self.llama3_405b_tokenizer}",
-                f"--tensor-parallelism-size={self.tensor_parallelism_size}",
-                f"--attention-kernel=torch",
-                f"--num-prompts={self.batch_size}",
-                f"--device={self.device}",
-            ]
-        )
-
-        baseline_mean_perplexity = round(
-            np.mean(baseline_perplexity["perplexities"][0 : self.batch_size]), 6
-        )
-        current_mean_perplexity = round(current_perplexity["mean_perplexity"], 6)
-
-        perplexity_difference = current_mean_perplexity - baseline_mean_perplexity
-
-        self.assertAlmostEqual(
-            baseline_mean_perplexity,
-            current_mean_perplexity,
-            delta=self.delta,
-            msg=f"Current perplexity deviates baseline by {perplexity_difference}",
-        )
-        gc.collect()
+        self.prepare_argv()
+        self.run_and_check_perplexity()
 
 
 if __name__ == "__main__":
