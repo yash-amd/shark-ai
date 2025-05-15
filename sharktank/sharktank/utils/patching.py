fd2c1fa8e746a19ec0fc9264fc335834a2199128
Sonbol Yazdanbakhsh <129094015+oyazdanb@users.noreply.github.com>
2025-05-15 11:16:36 -0400

    llama4 - patching (#1445)
    adding patching.py changes from Boian's llama4 branch 
    
    https://github.com/nod-ai/shark-ai/commit/0b8b714a01e67b9979b9c2037f9058a583b2a1b9#diff-0b44c2142e71c1fd626319f8a7e0032dea83540af9c077482c83c88d4425c539
    
    It adds tracing of arguments and nested arguments/results in a tree-like
    structure.

--- sharktank/sharktank/utils/patching.py
+++ sharktank/sharktank/utils/patching.py
@@ -5,8 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from pathlib import Path
-
+from typing import Any
+from collections.abc import Mapping, Iterable
+import logging
 import torch
+
+logger = logging.getLogger(__name__)
 
 
 class Patch:
@@ -16,7 +20,8 @@ class Patch:
         """Given a network, wraps the forward() method of children.
 
         Different types of callbacks can be specified to control wrapping:
-
+        * before_forward: Called with (module_name, module, args, kwarg) before
+        forward function. Used for logging inputs to a module.
         * after_forward: Called with (module_name, module, results) after the
         forward function returns. Used for logging results.
         """
@@ -25,6 +30,7 @@ class Patch:
             orig_forward = m.forward
 
             def wrapper(*args, **kwargs):
+                self.before_forward(name, m, args, kwargs)
                 results = orig_forward(*args, **kwargs)
                 self.after_forward(name, m, results)
                 return results
@@ -33,6 +39,16 @@ class Patch:
 
         for name, m in module.named_modules():
             _patch(name, m)
+
+    def before_forward(
+        self,
+        module_name: str,
+        module: torch.nn.Module,
+        args: list[Any],
+        kwargs: dict[str, Any],
+    ):
+        """Called before every patched forward() function."""
+        pass
 
     def after_forward(self, module_name: str, module: torch.nn.Module, results):
         """Called after every patched forward() function with results."""
@@ -50,30 +66,98 @@ class SaveModuleResultTensorsPatch(Patch):
     Users must call finalize() once all tensors have been accumulated.
     """
 
-    def __init__(self):
-        self.tensors = {}
+    def __init__(self, with_before_forward: bool = False):
+        self.with_before_forward = with_before_forward
+        self.tensors: dict[str, torch.Tensor] = {}
         # Map of module_name to last used index for duplicated tensors.
-        self.duplicate_tensors = {}
+        self.duplicate_tensors = dict[str, torch.Tensor] = {}
 
-    def after_forward(self, module_name: str, module: torch.nn.Module, results):
-        if not isinstance(results, torch.Tensor):
+    def before_forward(
+        self,
+        module_name: str,
+        module: torch.nn.Module,
+        args: list[Any],
+        kwargs: dict[str, Any],
+    ):
+        if not self.with_before_forward:
             return
 
-        result_tensor = torch.detach(results).contiguous().to(device="cpu").clone()
-        if module_name in self.tensors:
-            orig_dup = self.tensors[module_name]
-            del self.tensors[module_name]
-            self.duplicate_tensors[module_name] = 0
-            self.tensors[f"{module_name}#0"] = orig_dup
-        if module_name in self.duplicate_tensors:
-            index = self.duplicate_tensors[module_name] + 1
-            self.duplicate_tensors[module_name] = index
-            self.tensors[f"{module_name}#{index}"] = result_tensor
-        else:
-            self.tensors[module_name] = result_tensor
+        self._add_nested_tensors(
+            name_prefix=f"{module_name}.arg", tensors=args, name_delimiter="%"
+        )
+        self._add_nested_tensors(
+            name_prefix=f"{module_name}.arg", tensors=kwargs, name_delimiter="%"
+        )
 
-    def save_file(self, output_path: Path):
-        """Saves accumulated tensors to the given file."""
+    def after_forward(self, module_name: str, module: torch.nn.Module, results: Any):
+        self._add_nested_tensors(
+            name_prefix=module_name, tensors=results, name_delimiter="%"
+        )
+
+    def save_file(self, output_path: Path, *, skip_unsupported_dtypes: bool = False):
+        """Saves accumulated tensors to the given file.
+        Args:
+        skip_unsupported_dtypes:
+            skip tensors with dtype that is unsupported by safetensors.
+            Warn when such a tensor is encountered."""
         from safetensors.torch import save_file
 
-        save_file(self.tensors, output_path)
+        tensor_dict = self.tensors
+        if skip_unsupported_dtypes:
+            safetensors_unsupported_dtypes = set(
+                [torch.complex32, torch.complex64, torch.complex128]
+            )
+            unsupported_tensor_dict = {
+                k: v
+                for k, v in self.tensors.items()
+                if v.dtype in safetensors_unsupported_dtypes
+            }
+            if len(unsupported_tensor_dict) > 0:
+                unsupported_dtypes = {
+                    k: v.dtype for k, v in unsupported_tensor_dict.items()
+                }
+                logger.warning(
+                    f"Safetensors could not save tensor(s) with dtype {unsupported_dtypes}"
+                )
+                tensor_dict = {
+                    k: v
+                    for k, v in tensor_dict.items()
+                    if k not in unsupported_tensor_dict.keys()
+                }
+        save_file(tensor_dict, output_path)
+
+    def _add_nested_tensors(
+        self,
+        name_prefix: str,
+        tensors: list[Any] | dict[str, Any] | torch.Tensor,
+        name_delimiter: str,
+    ):
+        if isinstance(tensors, torch.Tensor):
+            self._add_tensor(name=name_prefix, tensor=tensors)
+        elif isinstance(tensors, Mapping):
+            for k, v in tensors.items():
+                self._add_nested_tensor(
+                    f"{name_prefix}{name_delimiter}{k}", v, name_delimiter
+                )
+        elif isinstance(tensors, Iterable):
+            for i, v in enumerate(tensors):
+                self._add_nested_tensor(
+                    f"{name_prefix}{name_delimiter}{i}", v, name_delimiter
+                )
+        else:
+            logger.warning(f"Could not handle element of type {type(tensors)}.")
+
+    def _add_tensor(self, name: str, tensor: torch.Tensor):
+        tensor = torch.detach(tensor).contiguous().to(device="cpu").clone()
+        if name in self.tensors:
+            orig_dup = self.tensors[name]
+            del self.tensors[name]
+            self.duplicate_tensors[name] = 0
+            self.tensors[f"{name}#0"] = orig_dup
+
+        if name in self.duplicate_tensors:
+            index = self.duplicate_tensors[name] + 1
+            self.duplicate_tensors[name] = index
+            self.tensors[f"{name}#{index}"] = tensor
+        else:
+            self.tensors[name] = tensor
