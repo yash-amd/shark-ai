cac925d966d33641b8a28cc8ff06179f6bdb735e
Rob Suderman <rob.suderman@gmail.com>
2025-05-21 12:21:08 -0700

    Reorder KV Cache contraction dimensions (#1465)
    The KV Cache should have contracting dimensions placed together.
    Specifically the block sequence stride should be after the KV heads but
    before the attn_dim.

--- sharktank/sharktank/layers/paged_attention.py
+++ sharktank/sharktank/layers/paged_attention.py
@@ -62,8 +62,8 @@ class KVCache:
         self.sub_page_dims = [
             self.transformer_block_count,
             self.cache_partition_count,
-            self.block_seq_stride,
             self.attn_head_count,
+            self.block_seq_stride,
             self.attn_head_dim,
         ]
 
@@ -125,8 +125,8 @@ class KVCache:
             bs,
             block_seq_len,
             self.cache_partition_count,
-            self.block_seq_stride,
             self.attn_head_count,
+            self.block_seq_stride,
             self.attn_head_dim,
         ]
 
@@ -140,6 +140,7 @@ class KVCache:
         selected = ops.index_select(page_table, 0, subblock_ids.flatten(0, 1))
 
         selected = selected.unflatten(0, blocked_shape[:2])
+        selected = selected.transpose(3, 4)
         key = selected[:, :, 0, :].flatten(1, 2)
         value = selected[:, :, 1, :].flatten(1, 2)
 
@@ -175,6 +176,7 @@ class KVCache:
                 1, (block_seq_len, self.block_seq_stride)
             )
             cache_partition = cache_partition.flatten(0, 1)
+            cache_partition = cache_partition.transpose(1, 2)
 
             part_block = ops.to(cache_partition, dtype=page_table.dtype)
             if page_table.dtype == torch.float8_e4m3fnuz:
@@ -198,25 +200,30 @@ class KVCache:
         assert len(cache_partitions) == self.cache_partition_count
 
         page_table = self.unflatten_page_table(state)[0]
-        page_table = page_table.flatten(0, 3)
+        page_table = page_table.flatten(0, 4)
 
         device = self.device
         bs, *_ = seq_positions.shape
 
         page_index = seq_positions // self.block_seq_stride
-        page_id = ops.gather(page_ids, dim=1, index=page_index.unsqueeze(1))
-        page_offset = (seq_positions % self.block_seq_stride).unsqueeze(1)
+        page_index = page_index.unsqueeze(1)
+        page_id = ops.gather(page_ids, dim=1, index=page_index).view((bs, 1, 1))
+        page_offset = (seq_positions % self.block_seq_stride).view((bs, 1, 1))
+        head_offset = torch.arange(self.attn_head_count).view(
+            (1, 1, self.attn_head_count)
+        )
 
         for cache_partition_id, cache_partition in enumerate(cache_partitions):
             # [1, 1]
-            partitions = torch.tensor(cache_partition_id, device=device).unsqueeze(0)
-            partitions = partitions.repeat(bs, 1)
+            partitions = torch.tensor(cache_partition_id, device=device).view((1, 1, 1))
 
             index = page_id
             index = index * self.transformer_block_count + transformer_block_index
             index = index * self.cache_partition_count + partitions
+            index = index * self.attn_head_count + head_offset
             index = index * self.block_seq_stride + page_offset
 
+            cache_partition.transpose(1, 2)
             values = ops.to(cache_partition, dtype=page_table.dtype)
 
             if isinstance(values, ShardedTensor) and type(values) != type(page_table):
@@ -274,8 +281,8 @@ class ShardedCache:
         self.unsharded_page_dims = [
             self.transformer_block_count,
             self.cache_partition_count,
-            self.block_seq_stride,
             self.attn_head_count,
+            self.block_seq_stride,
             self.attn_head_dim,
         ]
 
@@ -301,7 +308,7 @@ class ShardedCache:
         head_start = 0
         for cache in self.caches:
             head_end = head_start + cache.attn_head_count
-            shard = page_table[:, :, :, :, head_start:head_end]
+            shard = page_table[:, :, :, head_start:head_end]
             shard = shard.flatten(1)
             shards.append(shard)
             head_start = head_end
@@ -316,7 +323,7 @@ class ShardedCache:
             cache.unshard_state([shard])[0]
             for cache, shard in zip(self.caches, state[0].shards)
         ]
-        state = SplitPrimitiveTensor(ts=state, shard_dim=4, devices=self.devices)
+        state = SplitPrimitiveTensor(ts=state, shard_dim=3, devices=self.devices)
 
         return [ops.unshard(state)]
 
@@ -721,8 +728,8 @@ class PagedAttention:
 
     * transformer block
     * cache partition (K or V cache)
-    * block sequence stride (number of sequence positions per block)
     * attention heads
+    * block sequence stride (number of sequence positions per block)
     * attention dimensionality
 
     Note that the internal page structure matches the organization of the
