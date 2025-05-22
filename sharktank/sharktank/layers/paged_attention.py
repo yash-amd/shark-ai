3b83f5069be4ce39f970504f23227aceb2aa2c9f
Kunwar Grover <groverkss@gmail.com>
2025-05-22 19:06:57 +0100

    [PagedAttention] Add mlir_kernel for KVCache reads (#1481)
    Depends on https://github.com/nod-ai/shark-ai/pull/1477 and IREE bump to
    get the iree_linalg_ext.gather IR changes in.
    
    ---------
    
    Co-authored-by: Rob Suderman <rob.suderman@gmail.com>

--- sharktank/sharktank/layers/paged_attention.py
+++ sharktank/sharktank/layers/paged_attention.py
@@ -29,8 +29,97 @@ from sharktank.types import (
     StaticScaledQuantizer,
 )
 from sharktank import ops, kernels
+from sharktank.kernels.mlir_kernel import *
 
 __all__ = ["PagedAttention"]
+
+# Paged Attention Kernels
+#
+# Each kernel is put into its own class to create a namespace for it
+def KVCacheGatherKernel():
+    CACHE_SIZE = DynDim.CACHE_SIZE
+    PAGES = DynDim.PAGES
+    T_BLOCK = StaticDim.T_BLOCK
+    PART = StaticDim.PART
+    BLOCK_SEQ_STRIDE = StaticDim.BLOCK_SEQ_STRIDE
+    HEAD_COUNT_KV = StaticDim.HEAD_COUNT_KV
+    ATTN_HEAD_DIM = StaticDim.ATTN_HEAD_DIM
+    BATCH = DynDim.BATCH
+
+    CACHE_TY = Dtype.CACHE_TY
+    I64 = Dtype.I64
+
+    @mlir_kernel(
+        inputs=(
+            MLIRTensor[
+                CACHE_SIZE,
+                T_BLOCK,
+                PART,
+                HEAD_COUNT_KV,
+                BLOCK_SEQ_STRIDE,
+                ATTN_HEAD_DIM,
+                CACHE_TY,
+            ],
+            MLIRTensor[BATCH, PAGES, I64],
+            MLIRTensor[I64],
+            MLIRTensor[I64],
+        ),
+        results=(
+            MLIRTensor[
+                BATCH, PAGES, HEAD_COUNT_KV, BLOCK_SEQ_STRIDE, ATTN_HEAD_DIM, CACHE_TY
+            ],
+        ),
+    )
+    def paged_attention_kv_cache_gather(
+        cache, page_ids, transformer_idx, partition_idx, result
+    ):
+        mlir = """
+        !cache_slice = tensor<{{[CACHE_SIZE, HEAD_COUNT_KV, BLOCK_SEQ_STRIDE, ATTN_HEAD_DIM]|join('x')}}x!cache_dtype>
+
+        module {
+        util.func private @{{kernel_name}}(%cache: !cache,
+                                   %page_ids: !page_ids,
+                                   %transformer_idx: !transformer_idx,
+                                   %partition_idx: !partition_idx) -> !result {
+          %c0 = arith.constant 0 : index
+          %c1 = arith.constant 1 : index
+
+          // Get transformer/partition ids.
+          %t_id64 = tensor.extract %transformer_idx[] : !transformer_idx
+          %p_id64 = tensor.extract %partition_idx[] : !partition_idx
+          %t_id = arith.index_cast %t_id64 : !transformer_idx_dtype to index
+          %p_id = arith.index_cast %p_id64 : !partition_idx_dtype to index
+
+          // Get dynamic dimensions.
+          %cache_size = tensor.dim %cache, %c0 : !cache
+          %batches = tensor.dim %page_ids, %c0 : !page_ids
+          %pages = tensor.dim %page_ids, %c1 : !page_ids
+
+          // Extract a the current transformer block and partition from cache.
+          %cache_slice = tensor.extract_slice %cache
+            [0, %t_id, %p_id, 0, 0, 0]
+            [%cache_size, 1, 1, {{HEAD_COUNT_KV}}, {{BLOCK_SEQ_STRIDE}}, {{ATTN_HEAD_DIM}}]
+            [1, 1, 1, 1, 1, 1]
+            : !cache to !cache_slice
+
+          %empty = tensor.empty(%batches, %pages) : !result
+
+          // Gather from cache_slice using page_ids.
+          %result = iree_linalg_ext.gather
+                    dimension_map = [0]
+                    ins(%cache_slice, %page_ids : !cache_slice, !page_ids)
+                    outs(%empty : !result) -> !result
+
+          util.return %result : !result
+        }
+        }
+        """
+        return MLIRSpec(mlir)
+
+    return paged_attention_kv_cache_gather
+
+
+kv_cache_gather = KVCacheGatherKernel()
 
 
 class KVCache:
@@ -119,30 +208,25 @@ class KVCache:
     ):
         page_table = self.unflatten_page_table(state)[0]
 
-        bs, block_seq_len, *_ = page_ids.shape
-        # Blocks dim 1,2 according to the configured block stride.
-        blocked_shape = [
-            bs,
-            block_seq_len,
-            self.cache_partition_count,
-            self.attn_head_count,
-            self.block_seq_stride,
-            self.attn_head_dim,
-        ]
+        # TODO: mlir_kernel doesn't support non-tensor args yet, so use 0-D
+        # tensors instead.
+        t_id = torch.tensor(transformer_block_index, dtype=torch.int64)
+        key_p_id = torch.tensor(0, dtype=torch.int64)
+        value_p_id = torch.tensor(1, dtype=torch.int64)
 
-        # Gather both partitions and split post gather. This is more
-        # computationally efficient without gather fusion:
-        page_table = page_table.flatten(start_dim=0, end_dim=1)
-        transformer_block_index = torch.full(
-            (bs, block_seq_len), transformer_block_index, device=self.device
-        )
-        subblock_ids = page_ids * self.transformer_block_count + transformer_block_index
-        selected = ops.index_select(page_table, 0, subblock_ids.flatten(0, 1))
+        def unwrap_args(*ts):
+            new_ts = []
+            for t in ts:
+                if isinstance(t, DefaultPrimitiveTensor):
+                    t = t._data
+                new_ts.append(t)
+            return new_ts
 
-        selected = selected.unflatten(0, blocked_shape[:2])
-        selected = selected.transpose(3, 4)
-        key = selected[:, :, 0, :].flatten(1, 2)
-        value = selected[:, :, 1, :].flatten(1, 2)
+        key = kv_cache_gather(*unwrap_args(page_table, page_ids, t_id, key_p_id))
+        value = kv_cache_gather(*unwrap_args(page_table, page_ids, t_id, value_p_id))
+
+        key = key.transpose(2, 3).flatten(1, 2)
+        value = value.transpose(2, 3).flatten(1, 2)
 
         if self.devices:
             # Explicitly passing a list of one value to avoid redundant transfer inside ReplicateTensor.__init__.
